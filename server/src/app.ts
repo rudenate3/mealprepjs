a8e15f66957859c12002afd3dac242aa43c84432
rudenate3 <nathan.odin@gmail.com>
2021-07-09 13:09:19 -0400

    Add Morgan logging

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,8 +1,11 @@
-import express, { Request, Response } from 'express'
+import express, { Response } from 'express'
+
+import morgan from 'morgan'
 
 const app = express()
 
 if (process.env.NODE_ENV !== 'production') {
+  app.use(morgan('dev'))
   app.get('/up', (_, res: Response) => res.send({ status: 'Online' }))
 }
 
